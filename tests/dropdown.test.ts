d0552d106a7a1ce0150c94815beb3d2d742e551b
Gunjan Kanuga <kanugagunjan@gmail.com>
2024-07-02 00:19:28 +1200

    removed comments

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -4,12 +4,6 @@ test("alert", async ({ page }) => {
   await page.goto(
     "https://www.lambdatest.com/selenium-playground/select-dropdown-demo"
   );
-  //await page.selectOption("#select-demo", {
-  //label: "Monday",
-  //value: "Friday",
-  //index: 5,
-  //});
-  //await page.waitForTimeout(5000);
 
   await page.selectOption("#multi-select", [
     {
